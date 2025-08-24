5ca8839a04175f0d7a0e20f56836dd75f8dc7adb
middle gear <otukojoshua@outlook.com>
2025-08-24 23:07:40 +0300

    chore(clean): clean up

--- src/routes/anime/hianime.ts
+++ src/routes/anime/hianime.ts
@@ -6,98 +6,98 @@ import type { FastifyParams, FastifyQuery } from '../../utils/types.js';
 const zoro = new HiAnime();
 
 export default async function HianimeRoutes(fastify: FastifyInstance) {
-  fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
-    return reply.send({
-      message: 'Welcome to Hianime Provider',
-    });
-  });
+  // fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+  //   return reply.send({
+  //     message: 'Welcome to Hianime Provider',
+  //   });
+  // });
 
-  fastify.get('/search', async (request: FastifyRequest<{ Querystring: FastifyQuery }>, reply: FastifyReply) => {
-    let q = request.query.q?.trim() ?? '';
-    q = decodeURIComponent(q);
-    q = q.replace(/[^\w\s\-_.]/g, '');
+  // fastify.get('/search', async (request: FastifyRequest<{ Querystring: FastifyQuery }>, reply: FastifyReply) => {
+  //   let q = request.query.q?.trim() ?? '';
+  //   q = decodeURIComponent(q);
+  //   q = q.replace(/[^\w\s\-_.]/g, '');
 
-    if (!q.length) {
-      return reply.status(400).send({ error: 'Query string cannot be empty' });
-    }
-    if (q.length > 100) {
-      return reply.status(400).send({ error: 'Query too long' });
-    }
+  //   if (!q.length) {
+  //     return reply.status(400).send({ error: 'Query string cannot be empty' });
+  //   }
+  //   if (q.length > 100) {
+  //     return reply.status(400).send({ error: 'Query too long' });
+  //   }
 
-    const page = Number(request.query.page) || 1;
+  //   const page = Number(request.query.page) || 1;
 
-    reply.header('Cache-Control', 's-maxage=86400, stale-while-revalidate=300');
+  //   reply.header('Cache-Control', 's-maxage=86400, stale-while-revalidate=300');
 
-    const result = await zoro.search(q, page);
-    if ('error' in result) {
-      return reply.status(500).send({
-        error: result.error,
-        data: result.data,
-        hasNextPage: result.hasNextPage,
-        currentPage: result.currentPage,
-        lastPage: result.lastPage,
-      });
-    }
-    return reply.status(200).send({
-      hasNextPage: result.hasNextPage,
-      currentPage: result.currentPage,
-      lastPage: result.lastPage,
-      data: result.data,
-    });
-  });
+  //   const result = await zoro.search(q, page);
+  //   if ('error' in result) {
+  //     return reply.status(500).send({
+  //       error: result.error,
+  //       data: result.data,
+  //       hasNextPage: result.hasNextPage,
+  //       currentPage: result.currentPage,
+  //       lastPage: result.lastPage,
+  //     });
+  //   }
+  //   return reply.status(200).send({
+  //     hasNextPage: result.hasNextPage,
+  //     currentPage: result.currentPage,
+  //     lastPage: result.lastPage,
+  //     data: result.data,
+  //   });
+  // });
 
-  fastify.get('/info/:animeId', async (request: FastifyRequest<{ Params: FastifyParams }>, reply: FastifyReply) => {
-    const animeId = String(request.params.animeId);
+  // fastify.get('/info/:animeId', async (request: FastifyRequest<{ Params: FastifyParams }>, reply: FastifyReply) => {
+  //   const animeId = String(request.params.animeId);
 
-    reply.header('Cache-Control', 's-maxage=43200, stale-while-revalidate=300');
+  //   reply.header('Cache-Control', 's-maxage=43200, stale-while-revalidate=300');
 
-    const result = await zoro.fetchInfo(animeId);
-    if ('error' in result) {
-      return reply.status(500).send({
-        error: result.error,
-        data: result.data,
-      });
-    }
-    return reply.status(200).send({
-      data: result.data,
-    });
-  });
+  //   const result = await zoro.fetchInfo(animeId);
+  //   if ('error' in result) {
+  //     return reply.status(500).send({
+  //       error: result.error,
+  //       data: result.data,
+  //     });
+  //   }
+  //   return reply.status(200).send({
+  //     data: result.data,
+  //   });
+  // });
 
-  fastify.get('/episodes/:animeId', async (request: FastifyRequest<{ Params: FastifyParams }>, reply: FastifyReply) => {
-    const animeId = String(request.params.animeId);
+  // fastify.get('/episodes/:animeId', async (request: FastifyRequest<{ Params: FastifyParams }>, reply: FastifyReply) => {
+  //   const animeId = String(request.params.animeId);
 
-    reply.header('Cache-Control', 's-maxage=7200, stale-while-revalidate=300');
+  //   reply.header('Cache-Control', 's-maxage=7200, stale-while-revalidate=300');
 
-    const result = await zoro.fetchEpisodes(animeId);
+  //   const result = await zoro.fetchEpisodes(animeId);
 
-    if ('error' in result) {
-      return reply.status(500).send({
-        error: result.error,
-        data: result.data,
-      });
-    }
-    return reply.status(200).send({
-      data: result.data,
-    });
-  });
+  //   if ('error' in result) {
+  //     return reply.status(500).send({
+  //       error: result.error,
+  //       data: result.data,
+  //     });
+  //   }
+  //   return reply.status(200).send({
+  //     data: result.data,
+  //   });
+  // });
 
-  fastify.get('/servers/:episodeId', async (request: FastifyRequest<{ Params: FastifyParams }>, reply: FastifyReply) => {
-    const episodeId = String(request.params.episodeId);
+  // fastify.get('/servers/:episodeId', async (request: FastifyRequest<{ Params: FastifyParams }>, reply: FastifyReply) => {
+  //   const episodeId = String(request.params.episodeId);
 
-    reply.header('Cache-Control', 's-maxage=3600, stale-while-revalidate=300');
+  //   reply.header('Cache-Control', 's-maxage=3600, stale-while-revalidate=300');
 
-    const result = await zoro.fetchEpisodeServers(episodeId);
+  //   const result = await zoro.fetchEpisodeServers(episodeId);
 
-    if ('error' in result) {
-      return reply.status(500).send({
-        error: result.error,
-        data: result.data,
-      });
-    }
-    return reply.status(200).send({
-      data: result.data,
-    });
-  });
+  //   if ('error' in result) {
+  //     return reply.status(500).send({
+  //       error: result.error,
+  //       data: result.data,
+  //     });
+  //   }
+  //   return reply.status(200).send({
+  //     data: result.data,
+  //   });
+  // });
 
   fastify.get(
     '/watch/:episodeId',
--- src/routes/tv/flixhq.ts
+++ src/routes/tv/flixhq.ts
@@ -6,79 +6,79 @@ import { toFlixServers } from '../../utils/utils.js';
 const flixhq = new FlixHQ();
 
 export default async function FlixHQRoutes(fastify: FastifyInstance) {
-  fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
-    reply.send({ message: 'Welcome to FlixHQ provider' });
-  });
+  // fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+  //   reply.send({ message: 'Welcome to FlixHQ provider' });
+  // });
 
-  fastify.get('/search', async (request: FastifyRequest<{ Querystring: FastifyQuery }>, reply: FastifyReply) => {
-    let q = request.query.q?.trim() ?? '';
-    q = decodeURIComponent(q);
-    q = q.replace(/[^\w\s\-_.]/g, '');
+  // fastify.get('/search', async (request: FastifyRequest<{ Querystring: FastifyQuery }>, reply: FastifyReply) => {
+  //   let q = request.query.q?.trim() ?? '';
+  //   q = decodeURIComponent(q);
+  //   q = q.replace(/[^\w\s\-_.]/g, '');
 
-    if (q.length > 100) {
-      return reply.status(400).send({ error: 'Query string too long' });
-    }
-    if (!q.length) {
-      return reply.status(400).send({ error: 'Query string cannot be empty' });
-    }
+  //   if (q.length > 100) {
+  //     return reply.status(400).send({ error: 'Query string too long' });
+  //   }
+  //   if (!q.length) {
+  //     return reply.status(400).send({ error: 'Query string cannot be empty' });
+  //   }
 
-    reply.header('Cache-Control', 's-maxage=86400, stale-while-revalidate=300');
+  //   reply.header('Cache-Control', 's-maxage=86400, stale-while-revalidate=300');
 
-    const page = Number(request.query.page) || 1;
-    const result = await flixhq.search(q, page);
+  //   const page = Number(request.query.page) || 1;
+  //   const result = await flixhq.search(q, page);
 
-    if ('error' in result) {
-      return reply.status(500).send({
-        error: result.error,
-        data: result.data,
-        hasNextPage: result.hasNextPage,
-        currentPage: result.currentPage,
-      });
-    }
-    return reply.send({
-      currentPage: result.currentPage,
-      hasNextPage: result.hasNextPage,
-      data: result.data,
-    });
-  });
+  //   if ('error' in result) {
+  //     return reply.status(500).send({
+  //       error: result.error,
+  //       data: result.data,
+  //       hasNextPage: result.hasNextPage,
+  //       currentPage: result.currentPage,
+  //     });
+  //   }
+  //   return reply.send({
+  //     currentPage: result.currentPage,
+  //     hasNextPage: result.hasNextPage,
+  //     data: result.data,
+  //   });
+  // });
 
-  fastify.get('/info/:mediaId', async (request: FastifyRequest<{ Params: FastifyParams }>, reply: FastifyReply) => {
-    const mediaId = String(request.params.mediaId);
+  // fastify.get('/info/:mediaId', async (request: FastifyRequest<{ Params: FastifyParams }>, reply: FastifyReply) => {
+  //   const mediaId = String(request.params.mediaId);
 
-    reply.header('Cache-Control', 's-maxage=43200, stale-while-revalidate=300');
+  //   reply.header('Cache-Control', 's-maxage=43200, stale-while-revalidate=300');
 
-    const result = await flixhq.fetchMediaInfo(mediaId);
-    if ('error' in result) {
-      return reply.status(500).send({
-        error: result.error,
-        data: result.data,
-        episodes: result.episodes,
-      });
-    }
+  //   const result = await flixhq.fetchMediaInfo(mediaId);
+  //   if ('error' in result) {
+  //     return reply.status(500).send({
+  //       error: result.error,
+  //       data: result.data,
+  //       episodes: result.episodes,
+  //     });
+  //   }
 
-    return reply.send({
-      data: result.data,
-      episodes: result.episodes,
-    });
-  });
+  //   return reply.send({
+  //     data: result.data,
+  //     episodes: result.episodes,
+  //   });
+  // });
 
-  fastify.get('/servers/:episodeId', async (request: FastifyRequest<{ Params: FastifyParams }>, reply: FastifyReply) => {
-    const episodeId = String(request.params.episodeId);
+  // fastify.get('/servers/:episodeId', async (request: FastifyRequest<{ Params: FastifyParams }>, reply: FastifyReply) => {
+  //   const episodeId = String(request.params.episodeId);
 
-    reply.header('Cache-Control', 's-maxage=3600, stale-while-revalidate=300');
-    const results = await flixhq.fetchMediaServers(episodeId);
+  //   reply.header('Cache-Control', 's-maxage=3600, stale-while-revalidate=300');
+  //   const results = await flixhq.fetchMediaServers(episodeId);
 
-    if ('error' in results) {
-      return reply.status(500).send({
-        error: results.error,
-        data: results.data,
-      });
-    }
+  //   if ('error' in results) {
+  //     return reply.status(500).send({
+  //       error: results.error,
+  //       data: results.data,
+  //     });
+  //   }
 
-    return reply.send({
-      data: results.data,
-    });
-  });
+  //   return reply.send({
+  //     data: results.data,
+  //   });
+  // });
 
   fastify.get(
     '/watch/:episodeId',
