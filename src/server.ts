9067377d2b8e198b432404c0ee41648b9b9830dc
middle gear <otukojoshua@outlook.com>
2025-08-29 23:58:37 +0300

    feat(sentry): add sentry

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import Fastify, { type FastifyReply, type FastifyRequest } from 'fastify';
 
 import StaticRoutes from './routes/static.js';
-// import AnimekaiRoutes from './routes/anime/animekai.js';
+import AnimekaiRoutes from './routes/anime/animekai.js';
 import HianimeRoutes from './routes/anime/hianime.js';
 import AnilistRoutes from './routes/meta/anilist.js';
 import JikanRoutes from './routes/meta/jikan.js';
@@ -12,10 +12,47 @@ import TvMazeRoutes from './routes/meta/tvmaze.js';
 import { ratelimitOptions, rateLimitPlugIn } from './config/ratelimit.js';
 import fastifyCors, { corsOptions } from './config/cors.js';
 
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [nodeProfilingIntegration()],
+  enableLogs: true,
+  sendDefaultPii: true,
+  beforeSend: (event, hint) => {
+    const req = (hint.originalException as any)?.request;
+
+    if (req) {
+      const forwarded = req.headers?.['x-forwarded-for'];
+      const realIp = typeof forwarded === 'string' ? forwarded.split(',')[0]?.trim() : req.ip;
+
+      event.request = {
+        method: req.method,
+        url: req.url,
+        headers: req.headers,
+      };
+
+      event.user = {
+        ip_address: realIp,
+      };
+
+      event.extra = {
+        ...event.extra,
+        forwardedFor: forwarded,
+      };
+    }
+
+    return event;
+  },
+});
+
 const app = Fastify({
-  maxParamLength: 1000,
   logger: { level: 'info' },
 });
+
+// Hook Sentry into Fastify error lifecycle
+Sentry.setupFastifyErrorHandler(app);
 
 async function FastifyApp() {
   app.register(rateLimitPlugIn, ratelimitOptions);
@@ -31,7 +68,7 @@ async function FastifyApp() {
   await app.register(StaticRoutes);
   await app.register(AnilistRoutes, { prefix: '/api/anilist' });
   await app.register(JikanRoutes, { prefix: '/api/jikan' });
-  // await app.register(AnimekaiRoutes, { prefix: '/api/animekai' });
+  await app.register(AnimekaiRoutes, { prefix: '/api/animekai' });
   await app.register(HianimeRoutes, { prefix: '/api/hianime' });
   await app.register(FlixHQRoutes, { prefix: '/api/flixhq' });
   await app.register(TvMazeRoutes, { prefix: '/api/tvmaze' });
@@ -55,6 +92,7 @@ async function FastifyApp() {
 
 FastifyApp();
 
+// Vercel handler
 export default async function handler(req: FastifyRequest, res: FastifyReply) {
   await app.ready();
   app.server.emit('request', req, res);
